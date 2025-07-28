9b020474a93829d4eb6b2f89fa675d6c1fa00395
Natia Natisvili <natianatisvili@GLaDOS.local>
2025-07-28 15:52:15 +0200

    fixed unescaped HTML entities

--- src/components/MainHeading.js
+++ src/components/MainHeading.js
@@ -23,33 +23,34 @@ export default function MainHeading({ title, children }) {
               stagnation, where each year felt like a repetition of the last. In
               2021, I decided to pursue a long-held dream: learning to code and
               transitioning to the tech industry. While doing so, I maintained
-              part-time design work for a select clientele. Since then, I've
-              actively engaged in various courses and developed several small,
-              self-contained projects. At the end of 2024, I had the opportunity
-              to gain firsthand experience in the tech industry through an
-              internship, which allowed me to observe and participate in the
-              software development cycle.
+              part-time design work for a select clientele. Since then,
+              I&apos;ve actively engaged in various courses and developed
+              several small, self-contained projects. At the end of 2024, I had
+              the opportunity to gain firsthand experience in the tech industry
+              through an internship, which allowed me to observe and participate
+              in the software development cycle.
             </p>
             <p>
-              Currently, I'm actively expanding my skill set by developing
+              Currently, I&apos;m actively expanding my skill set by developing
               various web applications for private clientele. These include a
               joinery manufacturing list tool, informational and
               service-oriented websites for a bakery and for a ELD teacher,
               showcasing services and products. This work is not only about
-              writing code, it’s allowed me to immerse myself in full-stack
-              development, working across both frontend and backend systems. I’m
-              using tools such as Docker based databases, Testcontainers for
-              enhancing testing methodologies, and modern frameworks like
-              Next.js and Tailwind CSS to build scalable, production-ready
-              solutions. This hands-on experience continues to refine my ability
-              to deliver robust and maintainable software.
+              writing code, it&apos;s allowed me to immerse myself in full-stack
+              development, working across both frontend and backend systems.
+              I&apos;m using tools such as Docker based databases,
+              Testcontainers for enhancing testing methodologies, and modern
+              frameworks like Next.js and Tailwind CSS to build scalable,
+              production-ready solutions. This hands-on experience continues to
+              refine my ability to deliver robust and maintainable software.
             </p>
             <div className={styles.quoteContainer}>
               <blockquote>
                 <p>
                   A learning experience is one of those things that says,
                   <span>
-                    " You know that thing you just did? Don't do that."
+                    &quot; You know that thing you just did? Don&apos;t do
+                    that.&quot;
                   </span>
                 </p>
                 <cite>
