0b7780757e11a6740589f6a2a7317ed16b2ba8db
Natia Natisvili <natianatisvili@GLaDOS.local>
2025-07-31 15:58:13 +0200

    footer component finished

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,6 +20,7 @@ export default function RootLayout({ children }) {
             </aside>
             <main className={styles.main}>{children}</main>
           </div>
+
           <Footer />
         </div>
       </body>
